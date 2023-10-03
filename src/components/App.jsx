b90e9ce563129c49740586b87ec21d2d850eb4a6
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-03 22:58:43 +0300

    Dispath random greeting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,18 @@
 import '../App.css'
 import { Routes, Route } from 'react-router-dom'
 import Greeting from './Greeting'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchGreeting } from '../redux/GreetingsSplice'
 
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchGreeting())
+  }, [dispatch])
 
 
   return (
